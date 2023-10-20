722b0ef26aaa334e0a88c022a67fe3aaaee60cee
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-20 16:46:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const para  = document.getElementById("status");
 
 enterBtn.addEventListener('click',()=>{
 
-	// para.innerText = "";
+	para.innerText = "";
 
-	para.innerText = "Entered Metaverse";
-	para.style.fontSize = "50px";
+	// para.innerText = "Entered Metaverse";
+	// para.style.fontSize = "50px";
+
+	
+	const h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+
+	para.appendChild(h1);
 	
 })
